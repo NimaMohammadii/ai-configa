604b0c986078efd66f57325906dfcc45c4841fa0
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-05-29 02:56:44 +0330

    Add ElevenLabs voice list

---
+++ src/voices.js
@@ -0,0 +1,23 @@
+export const VOICES = {
+  Liam: "TX3LPaxmHKxFdv7VOQHJ",
+  Noah: "1SM7GgM6IMuvQlz2BwM3",
+  Ava: "tnSpp4vdxKPjI9w0GnoV",
+  Nora: "BIvP0GN1cAtSRTxNHnWS",
+  Alex: "GFGuOkimbpNkTEOVDkqX",
+  Ella: "NZiuR1C6kVMSWHG27sIM",
+  Chloe: "BZgkqPqms7Kj9ulSkVzn",
+  Alexandra: "kdmDKE6EkgrWrrykO9Qt",
+  Laura: "7piC4m7q8WrpEAnMj5xC",
+  Maxon: "0dPqNXnhg2bmxQv1WKDp",
+  Jessica: "cgSgspJ2msm6clMCkdW9",
+  Austin: "Bj9UqZbhQsanLzgalpEG",
+  priyanka: "BpjGufoPiobT79j2vtj4",
+  horatius: "qXpMhyvQqiRxWQs4qSSB",
+  anika: "Sm1seazb4gs7RSlUVw7c",
+  brock: "DGzg6RaUqxGRTHSBjfgF",
+  Xavier: "YOq2y2Up4RgXP2HyXjE5",
+  Lucas: "NNl6r8mD7vthiJatiJt1",
+};
+
+export const VOICE_NAMES = Object.keys(VOICES);
+export const VOICES_PER_PAGE = 9;
