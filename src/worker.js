7af34c66b21694754baf69d61d84438afe34a2f0
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-05-29 06:30:23 +0330

    Route Telegram Stars payment updates

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,5 @@
 import { handleCallback, handleMessage } from "./bot.js";
+import { handlePreCheckout, handleStarsCallback, handleStarsPayment, isStarsCallback } from "./stars-flow.js";
 
 export default {
   async fetch(request, env, ctx) {
@@ -13,12 +14,24 @@ export default {
     const update = await request.json().catch(() => null);
     if (!update) return new Response("Bad Request", { status: 400 });
 
+    if (update.pre_checkout_query) {
+      ctx.waitUntil(handlePreCheckout(update.pre_checkout_query, env).catch(logError));
+    }
+
     if (update.message) {
-      ctx.waitUntil(handleMessage(update.message, env).catch(logError));
+      if (update.message.successful_payment) {
+        ctx.waitUntil(handleStarsPayment(update.message, env).catch(logError));
+      } else {
+        ctx.waitUntil(handleMessage(update.message, env).catch(logError));
+      }
     }
 
     if (update.callback_query) {
-      ctx.waitUntil(handleCallback(update.callback_query, env).catch(logError));
+      if (isStarsCallback(update.callback_query.data)) {
+        ctx.waitUntil(handleStarsCallback(update.callback_query, env).catch(logError));
+      } else {
+        ctx.waitUntil(handleCallback(update.callback_query, env).catch(logError));
+      }
     }
 
     return new Response("OK");
