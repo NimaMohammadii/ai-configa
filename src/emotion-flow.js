952fcceafe1b89cdde983c6a9bc392752784f32d
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-06-17 18:50:01 +0330

    Add emotion flow placeholder

---
+++ src/emotion-flow.js
@@ -0,0 +1,9 @@
+export function isEmotionCallback(data) {
+  return data === "emotion_on";
+}
+
+export async function handleEmotionCallback() {}
+
+export async function handleEmotionMessage() {
+  return false;
+}
