96507dbe3687d4fb47534145db8af860952ed271
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-07-29 13:21:24 +0330

    Refresh keyboard dismiss glass style

--- src/mini-app/html.js
+++ src/mini-app/html.js
@@ -75,7 +75,7 @@ export const MINI_APP_HTML = `<!doctype html>
   <meta http-equiv="Pragma" content="no-cache"/>
   <meta http-equiv="Expires" content="0"/>
   <title>Vexa Voice</title>
-  <link rel="stylesheet" href="/mini-app/styles.css?v=20260729-ticket-glass-audio-edit-13"/>
+  <link rel="stylesheet" href="/mini-app/styles.css?v=20260729-ticket-glass-keyboard-14"/>
 </head>
 <body>
   <main class="app">
