47f189f2c9c55f421e88bef8223fdc00e6ebc6a3
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-08-07 10:59:28 +0330

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -5,7 +5,7 @@ import { VOICE_NAMES } from "./voices.js";
 
 export const CREDIT_PRICE_PER_1000_USD = 0.17;
 export const CREDIT_PER_CHARACTER = 1;
-export const TOMAN_PRICE_PER_1000 = 38000;
+export const TOMAN_PRICE_PER_1000 = 42000;
 export const TOMAN_MIN_PURCHASE_AMOUNT = 220000;
 export const MINIMAL_MIC_ICON = "🪼";
 
