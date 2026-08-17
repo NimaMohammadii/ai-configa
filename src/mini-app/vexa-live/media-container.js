a97cb315822384010a208389b7a8d1470994704f
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-08-17 12:32:16 +0330

    Restore Vexa media container runtime

---
+++ src/mini-app/vexa-live/media-container.js
@@ -0,0 +1,13 @@
+import { Container, getContainer } from "@cloudflare/containers";
+
+export class VexaMediaContainerV2 extends Container {
+  defaultPort = 8080;
+  sleepAfter = "10m";
+}
+
+export function getVexaMediaContainer(env, userId) {
+  if (!env.VEXA_MEDIA_CONTAINER) {
+    throw new Error("Vexa media container is unavailable");
+  }
+  return getContainer(env.VEXA_MEDIA_CONTAINER, "vexa-live-user-" + String(userId));
+}
