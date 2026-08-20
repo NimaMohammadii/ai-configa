af588356f56d03a554a4113fcbfb91ca864495b3
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-08-21 00:23:29 +0330

    Route Vexa Live through audited subtitle runtime

--- src/worker-youtube.js
+++ src/worker-youtube.js
@@ -25,7 +25,7 @@ import {
   appendVexaLiveSubtitlesRuntime,
   handleVexaLiveSubtitlesRequest,
   isVexaLiveSubtitlesRequest,
-} from "./mini-app/vexa-live/youtube-live-subtitles-v8.js";
+} from "./mini-app/vexa-live/youtube-live-subtitles-v9.js";
 import {
   appendVexaLivePersistenceRuntime,
   isVexaLivePersistenceRequest,
